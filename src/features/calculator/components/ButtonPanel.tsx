48110077a4ba635167b7894086b09cf604d3653c
damunro <dmunro@mines.edu>
2025-04-27 23:27:16 -0500

    made buttons betterere

--- src/features/calculator/components/ButtonPanel.tsx
+++ src/features/calculator/components/ButtonPanel.tsx
@@ -6,8 +6,12 @@ import { appendButtons } from "../constants/buttons";
 export default function ButtonPanel({ dispatch }: Readonly<Props>) {
 	return (
 		<SimpleGrid columns={4} py="4">
-			<Button onClick={() => dispatch({ type: "ENTER" })}>=</Button>
-			<Button onClick={() => dispatch({ type: "CLEAR" })}>C</Button>
+			<Button m="0.5" rounded="md" onClick={() => dispatch({ type: "ENTER" })}>
+				=
+			</Button>
+			<Button onClick={() => dispatch({ type: "CLEAR" })} m="0.5" rounded="md">
+				C
+			</Button>
 			{appendButtons.map((data) => (
 				<Button
 					key={data.symbol}

6a18f87bf75d542331f5419f7315a0a042ea4f8f
damunro <dmunro@mines.edu>
2025-05-07 20:05:53 -0500

    embedded page

--- src/app/projects/fuelcap/page.tsx
+++ src/app/projects/fuelcap/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Link, Stack } from "@chakra-ui/react";
+import { AspectRatio, Box, Heading, Link, Stack } from "@chakra-ui/react";
 
 import { RiGithubFill } from "react-icons/ri";
 import { Tooltip } from "@/components/ToolTip";
@@ -19,30 +19,51 @@ export default function FuelCap() {
 					</Link>
 				</Tooltip>
 			</Heading>
-			<Box maxW="1200px" mx="auto" borderRadius="2xl">
-				<div>Video to be added</div>
-				{/* <Box
-					as="video"
-					{...{
-						src: "/fuelcap.mp4",
-						controls: true,
-					}}
-					width="90%"
-					mx="auto"
-					borderRadius="2xl"
-				/> */}
-			</Box>
+			<AspectRatio
+				maxW="1200px"
+				minH="100px"
+				ratio={2}
+				borderRadius={"2xl"}
+				overflow="hidden"
+			>
+				<iframe
+					width="560"
+					height="315"
+					src="https://www.youtube.com/embed/DmKT0SBLmPo?si=LOZd5GlAGCTwNegP&rel=0"
+					title="YouTube video player"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					referrerPolicy="strict-origin-when-cross-origin"
+					allowFullScreen
+				></iframe>
+			</AspectRatio>
 			<Box
-				fontSize="2xl"
+				fontSize="xl"
 				bg="beige"
 				p={6}
 				borderRadius="2xl"
 				shadow="1em 1em 3em"
 			>
-				The goal of this project was to integrate a computer vision node on top
-				of an existing robotics stack. The project was built in ROS 2, creating
-				a node which essentially served as a wrapper for our machine learning
-				model
+				<p>
+					The goal of this project was to integrate a computer vision node on
+					top of an existing robotics stack. The project was built in ROS 2,
+					creating a node which essentially served as a wrapper for our machine
+					learning model.
+				</p>
+				<p>
+					It was our responsibility to take in depth image data, supplied by a
+					Realsense camera, and return a 6DOF pose estimation of a fuel cap, if
+					such a fuel cap exists in the image
+				</p>
+			</Box>
+			<Heading fontSize={"3xl"}>Problem Description</Heading>
+			<Box
+				fontSize="xl"
+				bg="beige"
+				p={6}
+				borderRadius="2xl"
+				shadow="1em 1em 3em"
+			>
+				Our task for my capstone project was to cre
 			</Box>
 			<Box
 				fontSize="2xl"

965d50ac340d27aa1648936b00502aa3efc81306
damunro <dmunro@mines.edu>
2025-05-07 19:34:01 -0500

    removed video

--- src/app/projects/fuelcap/page.tsx
+++ src/app/projects/fuelcap/page.tsx
@@ -3,6 +3,8 @@ import { Box, Heading, Link, Stack } from "@chakra-ui/react";
 import { RiGithubFill } from "react-icons/ri";
 import { Tooltip } from "@/components/ToolTip";
 
+// import { VideoHTMLAttributes } from "react";
+
 export default function FuelCap() {
 	return (
 		<Stack separator={<br />} w="80%">
@@ -17,6 +19,19 @@ export default function FuelCap() {
 					</Link>
 				</Tooltip>
 			</Heading>
+			<Box maxW="1200px" mx="auto" borderRadius="2xl">
+				<div>Video to be added</div>
+				{/* <Box
+					as="video"
+					{...{
+						src: "/fuelcap.mp4",
+						controls: true,
+					}}
+					width="90%"
+					mx="auto"
+					borderRadius="2xl"
+				/> */}
+			</Box>
 			<Box
 				fontSize="2xl"
 				bg="beige"
@@ -28,6 +43,16 @@ export default function FuelCap() {
 				of an existing robotics stack. The project was built in ROS 2, creating
 				a node which essentially served as a wrapper for our machine learning
 				model
+			</Box>
+			<Box
+				fontSize="2xl"
+				bg="tomato"
+				p={6}
+				borderRadius="2xl"
+				shadow="1em 1em 3em"
+			>
+				I will update more as I find it, it has been some time since I have
+				completed this project
 			</Box>
 		</Stack>
 	);
